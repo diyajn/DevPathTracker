bb92f7c97f7c509bd7f95afd55dc3aae64b98589
diyajn <diyajain341030@gmail.com>
2026-02-08 19:06:37 +0530

    first commit

---
+++ src/main/java/com/org/example/DevPathTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.org.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevPathTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevPathTrackerApplication.class, args);
+		System.out.println("Dev Path Tracker works!!");
+	}
+
+}
